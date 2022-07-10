6d9b1750143fcb7e9581e98090f91aaef9f238d4
Maksim Sobolev <sobmig@gmail.com>
2022-07-10 13:53:46 +0300

    завершено написание кода программы

---
+++ task37/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задать массив, осуществить в нем произведение парное умножение элементов (первого - с последним, 
+// второго - с предпоследним и т.д.). Результат вывести на экран новым массивом
+int [] createNewArray (int size, int start, int end)
+{
+    int [] massive = new int [size];
+    for (int j = 0; j < size; j++)
+    {
+        massive [j] = new Random().Next (start, end + 1);
+    }
+    return massive;
+}
+
+void printArray (int [] massive)
+{
+    int index = 0;
+    for (index = 0; index < massive.Length - 1; index++)
+    {
+        Console.Write (massive [index] + ", ");
+    }
+    if (index == massive.Length - 1)
+    {
+        Console.Write (massive [index]);
+    }
+}
+
+int [] NewArray (int [] massive)
+{
+    int sizeNewArray = 0;
+    int k = 1;
+    for (int i = 0; i < massive.Length - k; i++)
+    {
+        sizeNewArray = sizeNewArray + 1;
+        k++;
+    }
+    int [] result = new int [sizeNewArray];
+    for (int n = 0; n < result.Length; n++)
+    {
+        result [n] = massive [n] * massive [massive.Length - n - 1];
+    }
+    return result;
+}
+
+Console.WriteLine ("Введите размер массива: ");
+int length = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите начальное число диапазона чисел в массиве: ");
+int startNumber = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите конечное число диапазона чисел в массиве: ");
+int endNumber = Convert.ToInt32 (Console.ReadLine ());
+
+int [] array = createNewArray (length, startNumber, endNumber);
+Console.Write ("[");
+printArray (array);
+Console.Write ("] - > ");
+int [] array1 = NewArray (array);
+Console.Write ("[");
+printArray (array1);
+Console.Write ("]");
